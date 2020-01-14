9c77b4a0562daebbb294583fdfac426686e6d588
Park Seong Ho <glgl2131@inha.edu>
2020-01-14 10:02:29 +0900

    123123123123123123123

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 	
 	@RequestMapping("/")
 	String home() {		
-		return "젠킨스 테스11트3";
+		return "젠킨스 테스11121231231233트3";
 	}
 	
 	public static void main(String[] args) {
