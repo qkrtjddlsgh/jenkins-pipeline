fbbd6ef2f275fb64637a164260a0657075b72f38
Park Seong Ho <glgl2131@inha.edu>
2020-03-16 10:46:40 +0900

    docker registry update~~!

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 	
 	@RequestMapping("/")
 	String home() {		
-		return "Hello Kubernetes!!!";
+		return "CI/CD CI/CD";
 	}
 	
 	public static void main(String[] args) {
